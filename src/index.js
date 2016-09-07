3128baeda14bd2195b27091c36eb955a78f6dcfb
Adam Pash <adam.pash@gmail.com>
2016-09-07 11:01:02 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import Resource from './resource'
 import GenericExtractor from './extractor/generic'
 
 import fetchResource from './resource/utils/fetch-resource'
-// export default fetchResource
-
-// export { default as GenericExtractor } from './extractor/generic/index.js'
 
 const Iris = {
-  parse: async function(url) {
-    const $ = await Resource.create(url)
+  parse: async function(url, html) {
+    const $ = await Resource.create(url, html)
     const result = GenericExtractor.parse(url, null, $)
     return result
   }
