a86e63dca2fa2896a576adc50cdda600783f98f8
Adam Pash <adam.pash@gmail.com>
2016-10-27 17:08:34 -0700

    trying this

--- src/mercury.test.js
+++ src/mercury.test.js
@@ -5,19 +5,19 @@ import Mercury from './mercury';
 
 describe('Mercury', () => {
   describe('parse(url)', () => {
-    it('returns an error if a malformed url is passed', async function() {
+    it('returns an error if a malformed url is passed', async () => {
       const error = await Mercury.parse('foo.com');
 
       assert.equal(error, Errors.badUrl);
     });
 
-    it('returns an error if a bad url is passed', async function() {
+    it('returns an error if a bad url is passed', async () => {
       const error = await Mercury.parse('foo.com');
 
       assert.equal(error, Errors.badUrl);
     });
 
-    it('does the whole thing', async function() {
+    it('does the whole thing', async () => {
       const result =
         await Mercury.parse('http://deadspin.com/remember-when-donald-trump-got-booed-for-butchering-ta-1788216229');
 
@@ -25,33 +25,33 @@ describe('Mercury', () => {
       assert.equal(result.content.indexOf('score="') === -1, true);
     });
 
-    it('does blogger', async function() {
+    it('does blogger', async () => {
       const result =
         await Mercury.parse('https://googleblog.blogspot.com/2016/08/onhub-turns-one-today.html');
 
       assert.equal(typeof result, 'object');
     });
 
-    it('does blogger', async function() {
+    it('does blogger', async () => {
       const result = await Mercury.parse('https://googleblog.blogspot.com/2016/08/onhub-turns-one-today.html');
 
       assert.equal(typeof result, 'object');
     });
 
-    it('does wikipedia', async function() {
+    it('does wikipedia', async () => {
       const result = await Mercury.parse('https://en.wikipedia.org/wiki/Brihadeeswarar_Temple_fire');
 
       assert.equal(typeof result, 'object');
     });
 
-    it('does the nyt', async function() {
+    it('does the nyt', async () => {
       const result = await Mercury.parse('http://www.nytimes.com/2016/08/16/upshot/the-state-of-the-clinton-trump-race-is-it-over.html?_r=0');
 
       assert.equal(typeof result, 'object');
       assert.equal(result.total_pages, 1);
     });
 
-    it('does ars pagination', async function() {
+    it('does ars pagination', async () => {
       jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
       const url = 'http://arstechnica.com/gadgets/2016/08/the-connected-renter-how-to-make-your-apartment-smarter/';
       const result = await Mercury.parse(
