9d0a026e7318d9335bdaa130d4844c95c42b362f
Adam Pash <adam.pash@gmail.com>
2016-11-16 17:39:01 -0800

    karma.conf fix for ci

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    // autoWatch: true,
+    autoWatch: false,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -62,7 +62,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
