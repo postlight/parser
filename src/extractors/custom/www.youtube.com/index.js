01097e4e52223d4c3c49a7d0e27918efc708046b
Adam Pash <adam.pash@gmail.com>
2016-12-06 12:16:14 -0500

    feat: youtube custom extractor

---
+++ src/extractors/custom/www.youtube.com/index.js
@@ -0,0 +1,60 @@
+export const WwwYoutubeComExtractor = {
+  domain: 'www.youtube.com',
+
+  title: {
+    selectors: [
+      '.watch-title',
+      'h1.watch-title-container',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.yt-user-info',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[itemProp="datePublished"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    defaultCleaner: false,
+
+    selectors: [
+      ['#player-api', '#eow-description'],
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+      '#player-api': ($node, $) => {
+        const videoId = $('meta[itemProp="videoId"]').attr('value');
+        $node.html(`
+          <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`
+        );
+      },
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
