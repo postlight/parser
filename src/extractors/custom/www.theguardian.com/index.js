e7e41bd2420c2aa815857fb7547ca6305277cd0e
Janet <janetleekim@gmail.com>
2016-12-07 12:05:18 -0500

    feat: the guardian custom extractor (#41)

---
+++ src/extractors/custom/www.theguardian.com/index.js
@@ -0,0 +1,52 @@
+export const WwwTheguardianComExtractor = {
+  domain: 'www.theguardian.com',
+
+  title: {
+    selectors: [
+      '.content__headline',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'p.byline',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      '.content__standfirst',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.content__article-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.hide-on-mobile',
+      '.inline-icon',
+    ],
+  },
+};
