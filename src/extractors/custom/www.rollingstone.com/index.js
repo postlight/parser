d0280217e20bc586f92903cc5cfe2873bea62030
Janet <janetleekim@gmail.com>
2016-12-09 18:01:16 -0500

    rolling stone extractor
    This one was pretty good, but I had an issue with the date. Basically
    the date returned by the selector I used doesn’t match the date that I
    copied from the datePublished value in the jason in the head…

---
+++ src/extractors/custom/www.rollingstone.com/index.js
@@ -0,0 +1,51 @@
+export const WwwRollingstoneComExtractor = {
+  domain: 'www.rollingstone.com',
+
+  title: {
+    selectors: [
+      'h1.content-title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'a.content-author.tracked-offpage',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      'time.content-published-date',
+    ],
+  },
+
+  dek: {
+    selectors: [
+      '.content-description',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.article-content',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.module-related',
+    ],
+  },
+};
---
+++ src/extractors/custom/www.rollingstone.com/index.test.js
@@ -0,0 +1,97 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('WwwRollingstoneComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://www.rollingstone.com/movies/features/how-la-la-land-resurrected-the-hollywood-musical-w454198';
+      const html =
+        fs.readFileSync('./fixtures/www.rollingstone.com/1481322080542.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, '\'La La Land\': How a Young Filmmaker Resurrected the Hollywood Musical');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'David Fear');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2016-12-09T14:50:00.000Z');
+    });
+
+    it('returns the dek', async () => {
+    // To pass this test, fill out the dek selector
+    // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      const { dek } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(dek, 'Inside the story of how director Damien Chazelle and two movie stars attempted to modernize the genre – and made the movie of the year');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'http://img.wennermedia.com/social/rs-la-la-land-3d3a431a-8329-4539-b953-51e2d61a396c.jpg');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/www.rollingstone.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'Ask most folks what the most iconic thing about Los Angeles is, and');
+    });
+  });
+});
