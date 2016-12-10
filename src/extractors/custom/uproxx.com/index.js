3b78e20bd36c92c6ea1ab12acb8ea5930ce8adac
Janet <janetleekim@gmail.com>
2016-12-09 21:36:08 -0500

    uproxx extractor
    Issues:
    1) I couldn’t figure out how to extract the author name
    2) The preview looks okay, but the content appears to be different from
    both the live version of the article and the saved-copy. The
    discrepancy appears to be the Twitter embed. Not sure whether this is
    an issue.

---
+++ src/extractors/custom/uproxx.com/index.js
@@ -0,0 +1,45 @@
+export const UproxxComExtractor = {
+  domain: 'uproxx.com',
+
+  title: {
+    selectors: [
+      'div.post-top h1',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'span.authorname',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+       ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.post-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
