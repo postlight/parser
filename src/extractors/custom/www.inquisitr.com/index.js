82183ef05beb8f75f1492fe986b47aad5ffb03b4
Janet <janetleekim@gmail.com>
2016-12-13 16:53:50 -0500

    feat: inquisitor parser
    Date test failed because the format being parsed is different:
    AssertionError: '2016-12-12T16:07:20.000Z' ==
    '2016-12-12T16:07:20+00:00'
    
    Again, didn’t want to cheat.
    
    Image didn’t show in preview, but otherwise it looks good!

---
+++ src/extractors/custom/www.inquisitr.com/index.js
@@ -0,0 +1,45 @@
+export const WwwInquisitrComExtractor = {
+  domain: 'www.inquisitr.com',
+
+  title: {
+    selectors: [
+      'h1.entry-title.story--header--title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'div.story--header--author',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="datePublished"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div.text.entry-content.story--content',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
