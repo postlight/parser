612f168e9d3bfdc3c0e0338e93593250c7304037
Janet <janetleekim@gmail.com>
2016-12-13 18:05:36 -0500

    feat: chicago tribune parser
    Date is parsing but failing the test because:
    AssertionError: '2016-12-13T21:45:00.000Z' == '2016-12-13T13:45:00-0800'
    
    I tried to insert a line of code for Time Zone but I’m a n00b so I
    don’t think I did it right.
    
    No image showing up in the preview.

---
+++ src/extractors/custom/www.chicagotribune.com/index.js
@@ -0,0 +1,46 @@
+export const WwwChicagotribuneComExtractor = {
+  domain: 'www.chicagotribune.com',
+
+  title: {
+    selectors: [
+      'h1.trb_ar_hl_t',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'span.trb_ar_by_nm_au',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[itemprop="datePublished"]', 'value'],
+    ],
+    timezone: 'America/Chicago',
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div.trb_ar_page',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
