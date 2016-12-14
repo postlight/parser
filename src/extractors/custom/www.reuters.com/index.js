25e867db38fad6c90f9e96022390060a33cded3a
Janet <janetleekim@gmail.com>
2016-12-14 17:43:21 -0500

    feat: reuters parser
    Date parses correctly but fails test because of format discrepancy.
    
    Author tags are nested within the content, which is why the author
    names are appearing twice. I wasn’t sure how to address this.
    
    Additionally, the location appears twice, so I cleaned the location
    tags from the content.

---
+++ src/extractors/custom/www.reuters.com/index.js
@@ -0,0 +1,46 @@
+export const WwwReutersComExtractor = {
+  domain: 'www.reuters.com',
+
+  title: {
+    selectors: [
+      'h1.article-headline',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.author',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="og:article:published_time"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '#article-text',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      'span.location',
+      'span.articleLocation',
+    ],
+  },
+};
---
+++ src/extractors/custom/www.reuters.com/index.test.js
@@ -0,0 +1,87 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('WwwReutersComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://www.reuters.com/article/us-usa-fed-idUSKBN1430G4';
+      const html =
+        fs.readFileSync('./fixtures/www.reuters.com/1481754223635.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/www.reuters.com/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, 'Fed lifts rates, sees faster pace of hikes in Trump\'s first year');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/www.reuters.com/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'Howard Schneider and Lindsay Dunsmuir');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/www.reuters.com/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2016-12-14T22:03:42+0000');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/www.reuters.com/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'http://s2.reutersmedia.net/resources/r/?m=02&d=20161214&t=2&i=1165492293&w=&fh=545px&fw=&ll=&pl=&sq=&r=LYNXMPECBD1EH');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/www.reuters.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'WASHINGTON The U.S. Federal Reserve raised interest rates on Wednesday and signaled a');
+    });
+  });
+});
