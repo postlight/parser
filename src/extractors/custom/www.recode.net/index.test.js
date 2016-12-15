80b2d7db8e28603237d2e711f602bbdb70d7538e
Janet <janetleekim@gmail.com>
2016-12-15 17:43:35 -0500

    feat: recode parser
    Thumbs up, as far as I can tell.
    
    Note: No image appeared in the preview.

---
+++ src/extractors/custom/www.recode.net/index.test.js
@@ -0,0 +1,97 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('WwwRecodeNetExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://www.recode.net/2016/12/15/13971502/fcc-tom-wheeler-departure-net-neutrality-death-trump-layton';
+      const html =
+        fs.readFileSync('./fixtures/www.recode.net/1481841159344.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/www.recode.net/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, 'FCC chairman Tom Wheeler’s departure is the death knell for net neutrality');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/www.recode.net/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'April Glaser');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/www.recode.net/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2016-12-15T18:55:10.000Z');
+    });
+
+    it('returns the dek', async () => {
+    // To pass this test, fill out the dek selector
+    // in ./src/extractors/custom/www.recode.net/index.js.
+      const { dek } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(dek, 'The FCC will now have a Republican majority.');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/www.recode.net/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'https://cdn0.vox-cdn.com/thumbor/vYSadgIaWQTfllNFgfisldW3AFA=/0x30:2166x1248/1600x900/cdn0.vox-cdn.com/uploads/chorus_image/image/52310757/464506458.1481828109.jpeg');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/www.recode.net/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'U.S. Federal Communications Commission chairman Tom Wheeler announced this morning that he will');
+    });
+  });
+});
