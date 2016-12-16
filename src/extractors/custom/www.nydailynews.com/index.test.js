184de47d53b30fec827e0906f054610a2c429aa4
Janet <janetleekim@gmail.com>
2016-12-16 15:03:35 -0500

    feat: ny daily news parser
    The only issue is date formatting:
    AssertionError: '2016-12-16T18:38:14.000Z' == '2016-12-16T13:38:14-0500'

---
+++ src/extractors/custom/www.nydailynews.com/index.test.js
@@ -0,0 +1,87 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('WwwNydailynewsComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://www.nydailynews.com/news/politics/michelle-obama-feeling-not-hope-feels-article-1.2913255';
+      const html =
+        fs.readFileSync('./fixtures/www.nydailynews.com/1481917212613.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/www.nydailynews.com/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, 'Michelle Obama suggests America has lost hope since Donald Trump’s election: ‘We are feeling what not having hope feels like’');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/www.nydailynews.com/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'Meg Wagner');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/www.nydailynews.com/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2016-12-16T13:38:14-0500');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/www.nydailynews.com/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'http://assets.nydailynews.com/polopoly_fs/1.2913253.1481912929!/img/httpImage/image.jpg_gen/derivatives/landscape_1200/usa-trump.jpg');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/www.nydailynews.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'Michelle Obama understands your despair, America. The first lady suggested that the election');
+    });
+  });
+});
