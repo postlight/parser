a17f6a83c9c74c7bb490c2ae35c8238ac376b62b
Janet <janetleekim@gmail.com>
2016-12-16 15:46:49 -0500

    feat: natgeo parser
    For some reason, the local copy of the article didn’t grab the author
    name in it, so I couldn’t figure out how to parse it. The generic
    parser took a name of an author of a paper mentioned in the article,
    and thought that was the author name, which was funny.
    
    I cleaned a large block quote that didn’t make sense as it was shown in
    the preview, although I noticed that the Mercury chrome extension
    didn’t even display it.

---
+++ src/extractors/custom/news.nationalgeographic.com/index.test.js
@@ -0,0 +1,97 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('NewsNationalgeographicComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://news.nationalgeographic.com/energy/2015/08/150803-gender-bias-affects-office-heating-cooling-temperatures/';
+      const html =
+        fs.readFileSync('./fixtures/news.nationalgeographic.com/1481919545107.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, 'Here’s Why Your Office May Be Too Hot or Cold: Gender Bias');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'Wendy Koch');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2016-08-15T16:45:00.000Z');
+    });
+
+    it('returns the dek', async () => {
+    // To pass this test, fill out the dek selector
+    // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      const { dek } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(dek, 'Do you argue about the temperature in your office or home? Find out what often decides it, and tell us your preference.');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'http://news.nationalgeographic.com/content/dam/news/2015/08/03/temperaturegenderbias/02tempgenderbias.ngsversion.1438795800319.jpg');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/news.nationalgeographic.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'Many couples fight about it at home. No, it’s not money, sex, or');
+    });
+  });
+});
