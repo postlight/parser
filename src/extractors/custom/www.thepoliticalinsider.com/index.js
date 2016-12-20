7fdd74b91e34f0ff311f6de1f50cedb1d3264a6b
Janet <janetleekim@gmail.com>
2016-12-20 14:10:18 -0500

    feat: the political insider parser
    No problems here

---
+++ src/extractors/custom/www.thepoliticalinsider.com/index.js
@@ -0,0 +1,51 @@
+export const WwwThepoliticalinsiderComExtractor = {
+  domain: 'www.thepoliticalinsider.com',
+
+  title: {
+    selectors: [
+       ['meta[name="sailthru.title"]', 'value'],
+    ],
+  },
+
+  author: {
+    selectors: [
+      ['meta[name="sailthru.author"]', 'value'],
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="sailthru.date"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],  // enter selectors
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div#article-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
