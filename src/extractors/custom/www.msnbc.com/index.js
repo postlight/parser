871e41bb5560b0b855a81e6fadff7a9f85cb0a68
Janet <janetleekim@gmail.com>
2016-12-20 14:33:48 -0500

    feat: msnbc parser
    Everything seems good. Note that the image didn’t show up in the
    preview.

---
+++ src/extractors/custom/www.msnbc.com/index.js
@@ -0,0 +1,51 @@
+export const WwwMsnbcComExtractor = {
+  domain: 'www.msnbc.com',
+
+  title: {
+    selectors: [
+      'h1.is-title-pane',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.author',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="DC.date.issued"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div.panel-pane.pane-entity-field.pane-node-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
