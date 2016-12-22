c3d6b638811de2ba2144129513adf42569b89997
Janet <janetleekim@gmail.com>
2016-12-22 16:57:18 -0500

    feat: rawstory parser
    Finished, with a little help from Frankie (thanks Frankie!)

---
+++ src/extractors/custom/www.rawstory.com/index.js
@@ -0,0 +1,45 @@
+export const WwwRawstoryComExtractor = {
+  domain: 'www.rawstory.com',
+
+  title: {
+    selectors: [
+      '.blog-title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.blog-author a:first-of-type',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      '.blog-author a:last-of-type',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.blog-content',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
