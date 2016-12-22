52df5eca8302a4e87e9cc89fce97077d241293d5
Janet <janetleekim@gmail.com>
2016-12-22 13:42:06 -0500

    feat: cnet parser
    Date test fail - please take a look!
    
    Also, image didn’t load in preview.

---
+++ src/extractors/custom/www.cnet.com/index.js
@@ -0,0 +1,51 @@
+export const WwwCnetComExtractor = {
+  domain: 'www.cnet.com',
+
+  title: {
+    selectors: [
+      ['meta[name="og:title"]', 'value'],
+    ],
+  },
+
+  author: {
+    selectors: [
+      'a.author',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      'time',
+    ],
+  },
+
+  dek: {
+    selectors: [
+      '.article-dek',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.article-main-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
