877694d00009c85144849d7d80dd4b10189e2a5e
Janet <janetleekim@gmail.com>
2016-12-23 12:23:04 -0500

    feat: thepennyhoarder parser
    Looks good, although no image in preview!

---
+++ src/extractors/custom/www.thepennyhoarder.com/index.js
@@ -0,0 +1,45 @@
+export const WwwThepennyhoarderComExtractor = {
+  domain: 'www.thepennyhoarder.com',
+
+  title: {
+    selectors: [
+      ['meta[name="dcterms.title"]', 'value'],
+    ],
+  },
+
+  author: {
+    selectors: [
+      ['link[rel="author"]', 'title'],
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.post-text',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
