5fc5e52c05e4e5ad38c7e73a5dadd00d764b8c11
Janet <janetleekim@gmail.com>
2016-12-23 17:39:39 -0500

    feat: sciencefly extractor
    Everything is working okay but I had to use “twitter:image” rather than
    “og:image” because there were multiple instances of “og:image” and I
    wasn’t sure how to use first-of-type in this case. The image didn’t
    load in preview, but it might be related to the fact that I get a 404
    every time I try to load it.

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -38,3 +38,4 @@ export * from './uproxx.com';
 export * from './www.eonline.com';
 export * from './www.miamiherald.com';
 export * from './www.refinery29.com';
+export * from './sciencefly.com';
---
+++ src/extractors/custom/sciencefly.com/index.js
@@ -0,0 +1,53 @@
+export const ScienceflyComExtractor = {
+  domain: 'sciencefly.com',
+
+  title: {
+    selectors: [
+      '.entry-title',
+      '.cb-entry-title',
+      '.cb-single-title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'span.cb-title-fi div.cb-byline.cb-font-header div.cb-author.cb-byline-element',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="twitter:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div#article',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
