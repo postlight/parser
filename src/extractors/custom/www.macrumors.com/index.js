bea3919070515df419608ef2374cf64fe070eb84
Kevin Ngao <kevgong@yahoo.com>
2017-01-18 17:42:24 -0500

    feat: add macrumors

---
+++ src/extractors/custom/www.macrumors.com/index.js
@@ -0,0 +1,51 @@
+export const WwwMacrumorsComExtractor = {
+  domain: 'www.macrumors.com',
+
+  title: {
+    selectors: [
+      'h1.title'
+    ],
+  },
+
+  author: {
+    selectors: [
+      'a.author-url'
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      ['meta[name="description"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      'div.article'
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+
+    ],
+  },
+};
---
+++ src/extractors/custom/www.macrumors.com/index.test.js
@@ -0,0 +1,97 @@
+import assert from 'assert';
+import fs from 'fs';
+import URL from 'url';
+import cheerio from 'cheerio';
+
+import Mercury from 'mercury';
+import getExtractor from 'extractors/get-extractor';
+import { excerptContent } from 'utils/text';
+
+describe('WwwMacrumorsComExtractor', () => {
+  describe('initial test case', () => {
+    let result;
+    let url;
+    beforeAll(() => {
+      url =
+        'http://www.macrumors.com/2017/01/18/apple-pencil-2-march-2017/';
+      const html =
+        fs.readFileSync('./fixtures/www.macrumors.com/1484778558090.html');
+      result =
+        Mercury.parse(url, html, { fallback: false });
+    });
+
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
+      const extractor = getExtractor(url);
+      assert.equal(extractor.domain, URL.parse(url).hostname);
+    });
+
+    it('returns the title', async () => {
+    // To pass this test, fill out the title selector
+    // in ./src/extractors/custom/www.macrumors.com/index.js.
+      const { title } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(title, 'Second-Generation Apple Pencil Rumored for March of 2017');
+    });
+
+    it('returns the author', async () => {
+    // To pass this test, fill out the author selector
+    // in ./src/extractors/custom/www.macrumors.com/index.js.
+      const { author } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(author, 'Juli Clover');
+    });
+
+    it('returns the date_published', async () => {
+    // To pass this test, fill out the date_published selector
+    // in ./src/extractors/custom/www.macrumors.com/index.js.
+      const { date_published } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(date_published, '2017-01-18T05:00:00.000Z');
+    });
+
+    it('returns the dek', async () => {
+    // To pass this test, fill out the dek selector
+    // in ./src/extractors/custom/www.macrumors.com/index.js.
+      const { dek } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(dek, 'Apple may be planning to introduce a second-generation Apple Pencil this year, according to rumors out of the Asian supply chain. The new Apple...');
+    });
+
+    it('returns the lead_image_url', async () => {
+    // To pass this test, fill out the lead_image_url selector
+    // in ./src/extractors/custom/www.macrumors.com/index.js.
+      const { lead_image_url } = await result;
+
+    // Update these values with the expected values from
+    // the article.
+      assert.equal(lead_image_url, 'http://cdn.macrumors.com/article-new/2015/11/ipad-pro-apple-pencil-screen-800x471.jpg?retina');
+    });
+
+    it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/www.macrumors.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const first13 = excerptContent($('*').first().text(), 13);
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(first13, 'Apple may be planning to introduce a second-generation Apple Pencil this year, according');
+    });
+  });
+});
