6db25c77ed98375427c70646a4850fccdd95d817
Adam Pash <adam.pash@gmail.com>
2017-01-23 15:09:04 -0800

    transform .article-subtitle to h4; cleaning author but leaving location

--- src/extractors/custom/www.reuters.com/index.js
+++ src/extractors/custom/www.reuters.com/index.js
@@ -33,14 +33,16 @@ export const WwwReutersComExtractor = {
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
+      '.article-subtitle': 'h4',
     },
 
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
     // the result
     clean: [
-      'span.location',
-      'span.articleLocation',
+      '#article-byline .author',
+      // 'span.location',
+      // 'span.articleLocation',
     ],
   },
 };
