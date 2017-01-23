37c6096fd3df7b1f4e646c816cac4cd8f84cbfe6
Adam Pash <adam.pash@gmail.com>
2017-01-23 15:08:53 -0800

    test: fix date format

--- src/extractors/custom/www.reuters.com/index.test.js
+++ src/extractors/custom/www.reuters.com/index.test.js
@@ -55,7 +55,7 @@ describe('WwwReutersComExtractor', () => {
 
     // Update these values with the expected values from
     // the article.
-      assert.equal(date_published, '2016-12-14T22:03:42+0000');
+      assert.equal(date_published, '2016-12-14T22:03:42.000Z');
     });
 
     it('returns the lead_image_url', async () => {
