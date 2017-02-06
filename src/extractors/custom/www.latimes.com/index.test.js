a9b0a8c8ce4bf4b652e78578885296da11359654
Kevin Ngao <kevgong@yahoo.com>
2017-02-06 15:07:54 -0500

    fix: time test

--- src/extractors/custom/www.latimes.com/index.test.js
+++ src/extractors/custom/www.latimes.com/index.test.js
@@ -55,7 +55,7 @@ describe('WwwLatimesComExtractor', () => {
 
     // Update these values with the expected values from
     // the article.
-      assert.equal(date_published, '2016-12-16T05:00:00.000Z');
+      assert.equal(date_published, '2016-12-16T22:05:00.000Z');
     });
 
     it('returns the lead_image_url', async () => {
