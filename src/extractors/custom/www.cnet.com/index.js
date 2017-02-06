ba480a8ded63dd586eaf8f948cef460745522651
dviramontes <dviramontes@gmail.com>
2017-02-06 15:57:31 -0700

    fix: image lead

--- src/extractors/custom/www.cnet.com/index.js
+++ src/extractors/custom/www.cnet.com/index.js
@@ -35,12 +35,20 @@ export const WwwCnetComExtractor = {
 
   content: {
     selectors: [
+      ['img.__image-lead__', '.article-main-body'],
       '.article-main-body',
     ],
 
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
+      'figure.image': ($node) => {
+        const $img = $node.find('img');
+        $img.attr('width', '100%');
+        $img.attr('height', '100%');
+        $img.addClass('__image-lead__');
+        $node.remove('.imgContainer').prepend($img);
+      },
     },
 
     // Is there anything that is in the result that shouldn't be?
