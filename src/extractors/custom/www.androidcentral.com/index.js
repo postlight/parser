b4fec6af98401deae6e615c9a08f4a6a1faee3bb
Kevin Ngao <kevgong@yahoo.com>
2017-02-07 18:20:04 -0500

    feat: androidcentral parser (#119)
    * feat: androidcentral parser

---
+++ src/extractors/custom/www.androidcentral.com/index.js
@@ -0,0 +1,53 @@
+export const WwwAndroidcentralComExtractor = {
+  domain: 'www.androidcentral.com',
+
+  title: {
+    selectors: [
+      'h1',
+      'h1.main-title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.meta-by',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="article:published_time"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      ['meta[name="og:description"]', 'value'],
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['.image-large', 'src'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.article-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.intro',
+      'blockquote',
+    ],
+  },
+};
