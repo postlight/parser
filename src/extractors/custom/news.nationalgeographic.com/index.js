93d2baf5cf8a29a63485a1a90ae5f1240c9c432f
Janet <janetleekim@gmail.com>
2017-02-08 17:27:35 -0500

    feat: news.natgeo parser (#88)
    * feat: natgeo parser
    
    For some reason, the local copy of the article didn’t grab the author
    name in it, so I couldn’t figure out how to parse it. The generic
    parser took a name of an author of a paper mentioned in the article,
    and thought that was the author name, which was funny.
    
    I cleaned a large block quote that didn’t make sense as it was shown in
    the preview, although I noticed that the Mercury chrome extension
    didn’t even display it.
    
    * fix: add date_published transform
    
    * fix: date_published assertion
    
    * disable: author assertion, generlize author selector
    
    * rm: author assertion
    
    * fix: image lead
    
    * fix: guard agaist missing img url
    
    * fix: generalize dek and title selectors

---
+++ src/extractors/custom/news.nationalgeographic.com/index.js
@@ -0,0 +1,64 @@
+export const NewsNationalgeographicComExtractor = {
+  domain: 'news.nationalgeographic.com',
+
+  title: {
+    selectors: [
+      'h1',
+      'h1.main-title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.byline-component__contributors b span',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+       ['meta[name="article:published_time"]', 'value'],
+    ],
+    format: 'ddd MMM DD HH:mm:ss zz YYYY',
+    timezone: 'EST',
+  },
+
+  dek: {
+    selectors: [
+      '.article__deck',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      ['.parsys.content', '.__image-lead__'],
+      '.content',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+      '.parsys.content': ($node, $) => {
+        const $imgSrc = $node.find('.image.parbase.section')
+            .find('.picturefill')
+            .first()
+            .data('platform-src');
+        if ($imgSrc) {
+          $node.prepend($(`<img class="__image-lead__" src="${$imgSrc}"/>`));
+        }
+      },
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.pull-quote.pull-quote--large',
+    ],
+  },
+};
