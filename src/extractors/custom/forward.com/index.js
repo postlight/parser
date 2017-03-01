9c0c07e292a7bae18d0eb3641c02867a7a76a68b
Janet <janetleekim@gmail.com>
2017-03-01 13:51:34 -0500

    feat: forward.com parser
    LGTM although image didn’t show up in preview

---
+++ src/extractors/custom/forward.com/index.js
@@ -0,0 +1,53 @@
+export const ForwardComExtractor = {
+  domain: 'forward.com',
+
+  title: {
+    selectors: [
+      ['meta[name="sailthru.title"]', 'value'],
+    ],
+  },
+
+  author: {
+    selectors: [
+      ['meta[name="sailthru.author"]', 'value'],
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['meta[name="date"]', 'value'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      ['.post-item p'],
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      'div.donate-box',
+      'p.message',
+      'p.subtitle',
+    ],
+  },
+};
