c7bf2947a4aaf742e5634c77f95d5219eab7b633
Janet <janetleekim@gmail.com>
2017-03-01 20:42:59 -0500

    feat: qdaily parser
    Firstly — I accidentally tried to generate the parser on the master
    branch, and I’m not sure where it is, maybe floating in the nether
    world.
    
    On to the parser — this one was a bit tricky because things were in
    Chinese! The content appears to be parsing (as seen in preview) but
    it’s not passing the test. I noticed the second “ ‘ “ mark isn’t
    appearing on the parser side.
    
    Additionally, some of the lazy loading images aren’t appearing in the
    preview (I cleaned the wrong lazy load images that appeared), so
    someone will probably have to work on that (I don’t know how to do
    transforms yet).

---
+++ src/extractors/custom/www.qdaily.com/index.js
@@ -0,0 +1,52 @@
+export const WwwQdailyComExtractor = {
+  domain: 'www.qdaily.com',
+
+  title: {
+    selectors: [
+      'h2.title',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.name',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      ['span.date.smart-date', 'data-origindate'],
+    ],
+  },
+
+  dek: {
+    selectors: [
+      '.excerpt',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+    ['div.article-detail-hd img', 'src'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.detail',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.lazylad',
+      '.lazylood',
+    ],
+  },
+};
