dd6db9693566aa5cc8ff9ff109067b635b188e26
silasburton <silas.burton@gmail.com>
2017-03-09 11:08:35 -0500

    feat: gothamist extractor

---
+++ src/extractors/custom/gothamist.com/index.js
@@ -0,0 +1,60 @@
+export const GothamistComExtractor = {
+  domain: 'gothamist.com',
+
+  title: {
+    selectors: [
+      '.entry-header h1',
+    ],
+  },
+
+  author: {
+    selectors: [
+      '.author',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      'abbr.published',
+    ],
+  },
+
+  dek: {
+    selectors: [
+      null,
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.entry-body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+      'div.image-none': 'figure',
+      '.image-none i': 'figcaption',
+      'div.image-left': 'figure',
+      '.image-left i': 'figcaption',
+      'div.image-right': 'figure',
+      '.image-right i': 'figcaption',
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.image-none br',
+      '.image-left br',
+      '.image-right br',
+      '.galleryEase',
+    ],
+  },
+};
