11382ce651e32936d0c1c0fe00048ed2358ee800
Silas Burton <silas.burton@gmail.com>
2017-03-13 17:44:04 -0400

    Feat: Slate extractor (#153)
    * feat: slate extractor
    
    * fix: generalize selectors
    
    * fix: add Slate timezone

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -83,4 +83,5 @@ export * from './www.linkedin.com';
 export * from './obamawhitehouse.archives.gov';
 export * from './www.opposingviews.com';
 export * from './gothamist.com';
+export * from './www.slate.com';
 export * from './ici.radio-canada.ca';
---
+++ src/extractors/custom/www.slate.com/index.js
@@ -0,0 +1,57 @@
+export const WwwSlateComExtractor = {
+  domain: 'www.slate.com',
+
+  title: {
+    selectors: [
+      '.hed',
+      'h1',
+    ],
+  },
+
+  author: {
+    selectors: [
+      'a[rel=author]',
+    ],
+  },
+
+  date_published: {
+    selectors: [
+      '.pub-date',
+    ],
+
+    timezone: 'America/New_York',
+  },
+
+  dek: {
+    selectors: [
+      '.dek',
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [
+      ['meta[name="og:image"]', 'value'],
+    ],
+  },
+
+  content: {
+    selectors: [
+      '.body',
+    ],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {
+    },
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [
+      '.about-the-author',
+      '.pullquote',
+      '.newsletter-signup-component',
+      '.top-comment',
+    ],
+  },
+};
