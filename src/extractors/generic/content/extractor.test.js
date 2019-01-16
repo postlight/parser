33deb631c4a1109191385d764afb6c30c151f373
George Haddad <george.haddad@postlight.com>
2019-01-16 12:30:14 +0200

    dx: remove commented out code

--- src/extractors/generic/content/extractor.test.js
+++ src/extractors/generic/content/extractor.test.js
@@ -9,24 +9,15 @@ describe('GenericContentExtractor', () => {
   describe('extract($, html, opts)', () => {
     it('extracts html and returns the article', () => {
       const html = fs.readFileSync('./fixtures/vulture.html', 'utf-8');
-
-      // Array.from(range(1, 100)).map((i) => {
-      //   console.log(i)
-      //   clean(GenericContentExtractor.extract(
-      //     { $: null, html, url: 'http://example.com' }
-      //   ))
-      // })
       const result = clean(
         GenericContentExtractor.extract({
           $: null,
           html,
-          url:
-            'http://www.vulture.com/2016/08/dc-comics-greg-berlanti-c-v-r.html',
+          url: 'http://www.vulture.com/2016/08/dc-comics-greg-berlanti-c-v-r.html',
         })
       );
 
       assert(typeof result, 'string');
-      // console.log(result)
     });
   });
 });
