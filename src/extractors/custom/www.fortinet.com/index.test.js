de8e10b68c372e2adf0945c6f7a8bae48d4c4846
Wajeeh <wajeeh.zantout@gmail.com>
2019-01-17 16:54:40 +0200

    test: transform method

--- src/extractors/custom/www.fortinet.com/index.test.js
+++ src/extractors/custom/www.fortinet.com/index.test.js
@@ -14,10 +14,8 @@ describe('WwwFortinetComExtractor', () => {
     beforeAll(() => {
       url =
         'https://www.fortinet.com/blog/threat-research/defeating-an-android-packer-with-frida.html';
-      const html =
-        fs.readFileSync('./fixtures/www.fortinet.com/1546954846985.html');
-      result =
-        Mercury.parse(url, html, { fallback: false });
+      const html = fs.readFileSync('./fixtures/www.fortinet.com/1546954846985.html');
+      result = Mercury.parse(url, html, { fallback: false });
     });
 
     it('is selected properly', () => {
@@ -29,43 +27,46 @@ describe('WwwFortinetComExtractor', () => {
     });
 
     it('returns the title', async () => {
-    // To pass this test, fill out the title selector
-    // in ./src/extractors/custom/www.fortinet.com/index.js.
+      // To pass this test, fill out the title selector
+      // in ./src/extractors/custom/www.fortinet.com/index.js.
       const { title } = await result;
 
-    // Update these values with the expected values from
-    // the article.
+      // Update these values with the expected values from
+      // the article.
       assert.equal(title, 'How-to Guide: Defeating an Android Packer with FRIDA');
     });
 
     it('returns the author', async () => {
-    // To pass this test, fill out the author selector
-    // in ./src/extractors/custom/www.fortinet.com/index.js.
+      // To pass this test, fill out the author selector
+      // in ./src/extractors/custom/www.fortinet.com/index.js.
       const { author } = await result;
 
-    // Update these values with the expected values from
-    // the article.
+      // Update these values with the expected values from
+      // the article.
       assert.equal(author, 'Dario Durando');
     });
 
     it('returns the date_published', async () => {
-    // To pass this test, fill out the date_published selector
-    // in ./src/extractors/custom/www.fortinet.com/index.js.
+      // To pass this test, fill out the date_published selector
+      // in ./src/extractors/custom/www.fortinet.com/index.js.
       const { date_published } = await result;
 
-    // Update these values with the expected values from
-    // the article.
+      // Update these values with the expected values from
+      // the article.
       assert.equal(date_published, '2018-11-02T07:00:00.000Z');
     });
 
     it('returns the lead_image_url', async () => {
-    // To pass this test, fill out the lead_image_url selector
-    // in ./src/extractors/custom/www.fortinet.com/index.js.
+      // To pass this test, fill out the lead_image_url selector
+      // in ./src/extractors/custom/www.fortinet.com/index.js.
       const { lead_image_url } = await result;
 
-    // Update these values with the expected values from
-    // the article.
-      assert.equal(lead_image_url, 'https://www.fortinet.com/content/dam/fortinet-blog/article-images/defeating_an_android_packer_with_frida/frida_02.png');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(
+        lead_image_url,
+        'https://www.fortinet.com/content/dam/fortinet-blog/article-images/defeating_an_android_packer_with_frida/frida_02.png'
+      );
     });
 
     it('returns the content', async () => {
@@ -77,11 +78,33 @@ describe('WwwFortinetComExtractor', () => {
 
       const $ = cheerio.load(content || '');
 
-      const first13 = excerptContent($('*').first().text(), 13);
+      const first13 = excerptContent(
+        $('*')
+          .first()
+          .text(),
+        13
+      );
 
       // Update these values with the expected values from
       // the article.
-      assert.equal(first13, 'A FortiGuard Labs How-To Guide for Cybersecurity Threat Researchers Here at FortiGuard Labs');
+      assert.equal(
+        first13,
+        'A FortiGuard Labs How-To Guide for Cybersecurity Threat Researchers Here at FortiGuard Labs'
+      );
+    });
+
+    it('converts lazy-loaded noscript images to figures', async () => {
+      const { content } = await result;
+
+      const $ = cheerio.load(content || '');
+
+      const $lazyImages = $('noscript img');
+      const $figures = $('figure img');
+
+      // there were six lazy-loaded images
+      // noscript tags should be replaced by figures
+      assert.equal($lazyImages.length, 0);
+      assert.equal($figures.length, 6);
     });
   });
 });
