9415c7a03dec77a0ad7e8c91ad7deb19be952a6e
Wajeeh <wajeeh.zantout@gmail.com>
2019-01-17 16:54:14 +0200

    fix: transform method

--- src/extractors/custom/www.fortinet.com/index.js
+++ src/extractors/custom/www.fortinet.com/index.js
@@ -1,45 +1,31 @@
-import cheerio from 'cheerio';
-
 export const WwwFortinetComExtractor = {
   domain: 'www.fortinet.com',
 
   title: {
-    selectors: [
-      'h1',
-    ],
+    selectors: ['h1'],
   },
 
   author: {
-    selectors: [
-      '.b15-blog-meta__author',
-    ],
+    selectors: ['.b15-blog-meta__author'],
   },
 
   date_published: {
-    selectors: [
-      ['meta[name="article:published_time"]', 'value'],
-    ],
+    selectors: [['meta[name="article:published_time"]', 'value']],
   },
 
   lead_image_url: {
-    selectors: [
-      ['meta[name="og:image"]', 'value'],
-    ],
+    selectors: [['meta[name="og:image"]', 'value']],
   },
 
   content: {
-    selectors: [
-      'div.responsivegrid.aem-GridColumn.aem-GridColumn--default--12',
-    ],
+    selectors: ['div.responsivegrid.aem-GridColumn.aem-GridColumn--default--12'],
 
     transforms: {
       noscript: ($node) => {
-        const $ = cheerio.load(`<div>${$node.html()}</div>`);
-        const $children = $('div').children();
-        if ($children.length === 1 && $children.get(0).tagName === 'IMG') {
+        const $children = $node.children();
+        if ($children.length === 1 && $children.get(0).tagName === 'img') {
           return 'figure';
         }
-
         return null;
       },
     },
