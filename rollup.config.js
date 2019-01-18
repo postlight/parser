f0b32535b513bb2087febbe52d655440c022f226
Adam Pash <adam.pash@gmail.com>
2019-01-17 16:24:20 -0800

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup'; // eslint-disable-line import/extensions
 import commonjs from 'rollup-plugin-commonjs';
 
-const babelOpts = babelrc();
-babelOpts.runtimeHelpers = true;
+// const babelOpts = babelrc();
+// console.log(`babelOpts`, babelOpts);
+// babelOpts.runtimeHelpers = false;
 
 export default {
-  entry: 'src/mercury.js',
+  input: 'src/mercury.js',
   plugins: [
     commonjs(),
-    babel(babelOpts),
+    babel({
+      externalHelpers: false,
+      runtimeHelpers: true,
+    }),
   ],
-  format: 'cjs',
-  dest: process.env.MERCURY_TEST_BUILD ? 'dist/mercury_test.js' : 'dist/mercury.js',
-  sourceMap: true,
+  // format: 'cjs',
+  output: {
+    file: process.env.MERCURY_TEST_BUILD
+      ? 'dist/mercury_test.js'
+      : 'dist/mercury.js',
+    format: 'cjs',
+    sourceMap: true,
+  },
 };
--- rollup.config.web.js
+++ rollup.config.web.js
@@ -1,19 +1,49 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup'; // eslint-disable-line import/extensions
-import commonjs from 'rollup-plugin-commonjs';
+// #<{(| eslint-disable import/no-extraneous-dependencies |)}>#
+// import babel from 'rollup-plugin-babel';
+// import babelrc from 'babelrc-rollup'; // eslint-disable-line import/extensions
+// import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import globals from 'rollup-plugin-node-globals';
-import uglify from 'rollup-plugin-uglify'; // eslint-disable-line import/extensions
+// import { uglify } from 'rollup-plugin-uglify'; // eslint-disable-line import/extensions
+//
+// const babelOpts = babelrc();
+// babelOpts.runtimeHelpers = true;
+// babelOpts.exclude = './node_modules#<{(|*';
+//
+// export default {
+//   entry: 'src/mercury.js',
+//   plugins: [
+//     babel(babelOpts),
+//     commonjs({
+//       ignoreGlobal: true,
+//     }),
+//     globals(),
+//     nodeResolve({
+//       browser: true,
+//       preferBuiltins: false,
+//     }),
+//     uglify(),
+//   ],
+//   format: 'iife',
+//   moduleName: 'Mercury',
+//   dest: process.env.MERCURY_TEST_BUILD ? 'dist/mercury_test.web.js' : 'dist/mercury.web.js',
+//   sourceMap: false,
+// };
+/* eslint-disable import/no-extraneous-dependencies */
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 
-const babelOpts = babelrc();
-babelOpts.runtimeHelpers = true;
-babelOpts.exclude = './node_modules/**';
+// const babelOpts = babelrc();
+// console.log(`babelOpts`, babelOpts);
+// babelOpts.runtimeHelpers = false;
 
 export default {
-  entry: 'src/mercury.js',
+  input: 'src/mercury.js',
   plugins: [
-    babel(babelOpts),
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**',
+    }),
     commonjs({
       ignoreGlobal: true,
     }),
@@ -22,10 +52,14 @@ export default {
       browser: true,
       preferBuiltins: false,
     }),
-    uglify(),
+    // uglify(),
   ],
-  format: 'iife',
-  moduleName: 'Mercury',
-  dest: process.env.MERCURY_TEST_BUILD ? 'dist/mercury_test.web.js' : 'dist/mercury.web.js',
-  sourceMap: false,
+  output: {
+    file: process.env.MERCURY_TEST_BUILD
+      ? 'dist/mercury_test.web.js'
+      : 'dist/mercury.web.js',
+    format: 'iife',
+    name: 'Mercury',
+    sourceMap: true,
+  },
 };
