20de84efd4bb38ae86fdc64c16c2f082a77680ac
Adam Pash <adam.pash@gmail.com>
2019-01-18 11:34:24 -0800

    add back uglify

--- rollup.config.web.js
+++ rollup.config.web.js
@@ -1,35 +1,6 @@
-// #<{(| eslint-disable import/no-extraneous-dependencies |)}>#
-// import babel from 'rollup-plugin-babel';
-// import babelrc from 'babelrc-rollup'; // eslint-disable-line import/extensions
-// import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import globals from 'rollup-plugin-node-globals';
-// import { uglify } from 'rollup-plugin-uglify'; // eslint-disable-line import/extensions
-//
-// const babelOpts = babelrc();
-// babelOpts.runtimeHelpers = true;
-// babelOpts.exclude = './node_modules#<{(|*';
-//
-// export default {
-//   entry: 'src/mercury.js',
-//   plugins: [
-//     babel(babelOpts),
-//     commonjs({
-//       ignoreGlobal: true,
-//     }),
-//     globals(),
-//     nodeResolve({
-//       browser: true,
-//       preferBuiltins: false,
-//     }),
-//     uglify(),
-//   ],
-//   format: 'iife',
-//   moduleName: 'Mercury',
-//   dest: process.env.MERCURY_TEST_BUILD ? 'dist/mercury_test.web.js' : 'dist/mercury.web.js',
-//   sourceMap: false,
-// };
-/* eslint-disable import/no-extraneous-dependencies */
+import { uglify } from 'rollup-plugin-uglify'; // eslint-disable-line import/extensions
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
@@ -48,7 +19,7 @@ export default {
       browser: true,
       preferBuiltins: false,
     }),
-    // uglify(),
+    uglify(),
   ],
   output: {
     file: process.env.MERCURY_TEST_BUILD
