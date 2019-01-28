bb6ad2682b661723baf89c34f0e0a899c42ceafb
Toufic Mouallem <touficmouallem@gmail.com>
2019-01-28 19:31:52 +0200

    fix: Transform relative URLs in srcset attributes to absolute URLs (#190)

--- src/utils/dom/make-links-absolute.js
+++ src/utils/dom/make-links-absolute.js
@@ -14,8 +14,37 @@ function absolutize($, rootUrl, attr, $content) {
   });
 }
 
+function absolutizeSet($, rootUrl, $content) {
+  $('[srcset]', $content).each((_, node) => {
+    const attrs = getAttrs(node);
+    const urlSet = attrs.srcset;
+
+    if (urlSet) {
+      // a comma should be considered part of the candidate URL unless preceded by a descriptor
+      // descriptors can only contain positive numbers followed immediately by either 'w' or 'x'
+      // space characters inside the URL should be encoded (%20 or +)
+      const candidates = urlSet.match(
+        /(?:\s*)(\S+(?:\s*[\d.]+[wx])?)(?:\s*,\s*)?/g
+      );
+      const absoluteCandidates = candidates.map(candidate => {
+        // a candidate URL cannot start or end with a comma
+        // descriptors are separated from the URLs by unescaped whitespace
+        const parts = candidate
+          .trim()
+          .replace(/,$/, '')
+          .split(/\s+/);
+        parts[0] = URL.resolve(rootUrl, parts[0]);
+        return parts.join(' ');
+      });
+      const absoluteUrlSet = [...new Set(absoluteCandidates)].join(', ');
+      setAttr(node, 'srcset', absoluteUrlSet);
+    }
+  });
+}
+
 export default function makeLinksAbsolute($content, $, url) {
   ['href', 'src'].forEach(attr => absolutize($, url, attr, $content));
+  absolutizeSet($, url, $content);
 
   return $content;
 }
