c6c70825309616e9f05787e9a5926287c785b2a6
George Haddad <george.haddad@postlight.com>
2019-01-30 10:29:40 +0200

    fix: use ISO time format and preserve original timezone offset

--- src/extractors/custom/www.fastcompany.com/index.test.js
+++ src/extractors/custom/www.fastcompany.com/index.test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import URL from 'url';
 import cheerio from 'cheerio';
+import moment from 'moment';
 
 import Mercury from 'mercury';
 import getExtractor from 'extractors/get-extractor';
@@ -15,10 +16,10 @@ describe('WwwFastcompanyComExtractor', () => {
     beforeAll(() => {
       url =
         'https://www.fastcompany.com/3067012/the-only-five-email-folders-your-inbox-will-ever-need';
-      const html =
-        fs.readFileSync('./fixtures/www.fastcompany.com/1547124373499.html');
-      result =
-        Mercury.parse(url, html, { fallback: false });
+      const html = fs.readFileSync(
+        './fixtures/www.fastcompany.com/1547124373499.html'
+      );
+      result = Mercury.parse(url, html, { fallback: false });
     });
 
     it('is selected properly', () => {
@@ -30,53 +31,63 @@ describe('WwwFastcompanyComExtractor', () => {
     });
 
     it('returns the title', async () => {
-    // To pass this test, fill out the title selector
-    // in ./src/extractors/custom/www.fastcompany.com/index.js.
+      // To pass this test, fill out the title selector
+      // in ./src/extractors/custom/www.fastcompany.com/index.js.
       const { title } = await result;
 
-    // Update these values with the expected values from
-    // the article.
-      assert.equal(title, 'The Only Five Email Folders Your Inbox Will Ever Need');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(
+        title,
+        'The Only Five Email Folders Your Inbox Will Ever Need'
+      );
     });
 
     it('returns the author', async () => {
-    // To pass this test, fill out the author selector
-    // in ./src/extractors/custom/www.fastcompany.com/index.js.
+      // To pass this test, fill out the author selector
+      // in ./src/extractors/custom/www.fastcompany.com/index.js.
       const { author } = await result;
 
-    // Update these values with the expected values from
-    // the article.
+      // Update these values with the expected values from
+      // the article.
       assert.equal(author, 'Zach Hanlon');
     });
 
     it('returns the date_published', async () => {
-    // To pass this test, fill out the date_published selector
-    // in ./src/extractors/custom/www.fastcompany.com/index.js.
+      // To pass this test, fill out the date_published selector
+      // in ./src/extractors/custom/www.fastcompany.com/index.js.
       const { date_published } = await result;
+      const newDatePublished = moment(date_published).toISOString(true);
 
-    // Update these values with the expected values from
-    // the article.
-      assert.equal(date_published, '2017-01-09T05:00:00.000Z');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(newDatePublished, '2017-01-09T05:00:00.000+02:00');
     });
 
     it('returns the dek', async () => {
-    // To pass this test, fill out the dek selector
-    // in ./src/extractors/custom/www.fastcompany.com/index.js.
+      // To pass this test, fill out the dek selector
+      // in ./src/extractors/custom/www.fastcompany.com/index.js.
       const { dek } = await result;
 
-    // Update these values with the expected values from
-    // the article.
-      assert.equal(dek, 'Stop “organizing” your emails by subject and start thinking of them in terms of deadlines.');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(
+        dek,
+        'Stop “organizing” your emails by subject and start thinking of them in terms of deadlines.'
+      );
     });
 
     it('returns the lead_image_url', async () => {
-    // To pass this test, fill out the lead_image_url selector
-    // in ./src/extractors/custom/www.fastcompany.com/index.js.
+      // To pass this test, fill out the lead_image_url selector
+      // in ./src/extractors/custom/www.fastcompany.com/index.js.
       const { lead_image_url } = await result;
 
-    // Update these values with the expected values from
-    // the article.
-      assert.equal(lead_image_url, 'https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/fc/3067012-poster-p-1-the-only-five-email-folders-your-inbox-will-ever-need.jpg');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(
+        lead_image_url,
+        'https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/fc/3067012-poster-p-1-the-only-five-email-folders-your-inbox-will-ever-need.jpg'
+      );
     });
 
     it('returns the content', async () => {
@@ -88,11 +99,19 @@ describe('WwwFastcompanyComExtractor', () => {
 
       const $ = cheerio.load(content || '');
 
-      const first13 = excerptContent($('*').first().text(), 13);
+      const first13 = excerptContent(
+        $('*')
+          .first()
+          .text(),
+        13
+      );
 
       // Update these values with the expected values from
       // the article.
-      assert.equal(first13, 'For years, my approach to email was like slaying a hydra. For every');
+      assert.equal(
+        first13,
+        'For years, my approach to email was like slaying a hydra. For every'
+      );
     });
   });
 });
