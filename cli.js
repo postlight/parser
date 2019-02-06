95025bf4573702cac690306bdbd0507f49dc6b07
Adam Pash <adam.pash@gmail.com>
2019-02-06 09:24:04 -0800

    feat: handle cli errors/timeout
    Closes #248

--- cli.js
+++ cli.js
@@ -19,6 +19,23 @@ Usage:\n\
     );
     return;
   }
-  const result = await Mercury.parse(urlToParse);
-  console.log(JSON.stringify(result, null, 2));
+  try {
+    const result = await Mercury.parse(urlToParse);
+    console.log(JSON.stringify(result, null, 2));
+  } catch (e) {
+    const reportBug =
+      'If you believe this was an error, please file an issue at:\n\n    https://github.com/postlight/mercury-parser/issues/new';
+    if (e.message === 'ETIMEDOUT') {
+      console.error(
+        `\nMercury Parser encountered a timeout trying to load that resource.\n\n${reportBug}\n`
+      );
+    } else {
+      console.error(
+        '\nMercury Parser encountered an error trying to parse that resource.\n\n'
+      );
+      console.error(e);
+      console.error(`\n\n${reportBug}\n`);
+    }
+    process.exit(1);
+  }
 })(url);
