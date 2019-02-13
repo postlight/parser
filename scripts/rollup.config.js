ab56ce0de3d789a19321f06c35fadc5f55a2108e
Adam Pash <adam.pash@gmail.com>
2019-02-12 16:14:47 -0800

    fix: custom parser generator (#271)
    - swap fs import
    - fix rollup config

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,15 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup'; // eslint-disable-line import/extensions
 import commonjs from 'rollup-plugin-commonjs';
 
-const babelOpts = babelrc();
-babelOpts.runtimeHelpers = true;
-
 export default {
-  entry: './scripts/generate-custom-parser.js',
-  plugins: [commonjs(), babel(babelOpts)],
-  format: 'cjs',
-  dest: 'dist/generate-custom-parser.js', // equivalent to --output
-  sourceMap: true,
+  input: 'scripts/generate-custom-parser.js',
+  plugins: [
+    commonjs(),
+    babel({
+      externalHelpers: false,
+      runtimeHelpers: true,
+    }),
+  ],
+  treeshake: true,
+  output: {
+    file: 'dist/generate-custom-parser.js',
+    format: 'cjs',
+    sourceMap: true,
+  },
 };
