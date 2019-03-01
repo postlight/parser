40f2a25d9dbc1c5dbfa4789128ca3bbd173ebb72
kik0220 <kik0220@gmail.com>
2019-03-01 10:50:24 +0900

    fix: paste miss... (#287)

--- src/extractors/custom/news.mynavi.jp/index.js
+++ src/extractors/custom/news.mynavi.jp/index.js
@@ -22,7 +22,7 @@ export const NewsMynaviJpExtractor = {
   },
 
   content: {
-    selectors: [['meta[name="og:image"]', 'value']],
+    selectors: ['main article div'],
 
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
