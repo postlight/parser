d1b0a5a2da6c27185244bfe0b5739bd37e58de4f
David Leong <david0leong@gmail.com>
2019-03-04 08:00:26 -0800

    Fix date_published test

--- src/extractors/custom/clinicaltrials.gov/index.test.js
+++ src/extractors/custom/clinicaltrials.gov/index.test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import URL from 'url';
 import cheerio from 'cheerio';
+import moment from 'moment-timezone';
 
 import Mercury from 'mercury';
 import getExtractor from 'extractors/get-extractor';
@@ -59,7 +60,7 @@ describe('ClinicaltrialsGovExtractor', () => {
 
       // Update these values with the expected values from
       // the article.
-      assert.equal(date_published, '2018-11-21T08:00:00.000Z');
+      assert.equal(moment(date_published).format('YYYY-MM-DD'), '2018-11-21');
     });
 
     it('returns the content', async () => {
