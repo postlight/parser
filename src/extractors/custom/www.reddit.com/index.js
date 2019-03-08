76d4b49341b99b0c2f6b8bdf5610ddf65aeb9f1a
Toufic Mouallem <touficmouallem@gmail.com>
2019-03-08 21:13:45 +0200

    feat: Add support for all Reddit post types

--- src/extractors/custom/www.reddit.com/index.js
+++ src/extractors/custom/www.reddit.com/index.js
@@ -20,20 +20,37 @@ export const WwwRedditComExtractor = {
   },
 
   content: {
-    selectors: ['div[data-test-id="post-content"]'],
+    selectors: [
+      ['div[data-test-id="post-content"] p'], // text post
+      [
+        'div[data-test-id="post-content"] a[target="_blank"]:not([data-click-id="timestamp"])', // external link
+        'div[data-test-id="post-content"] div[data-click-id="media"]', // embedded media
+      ], // external link with media preview (YouTube, imgur album, etc...)
+      ['div[data-test-id="post-content"] div[data-click-id="media"]'], // Embedded media (Reddit video)
+      [
+        'div[data-test-id="post-content"] a[target="_blank"]:not([data-click-id="timestamp"])',
+      ], // external link
+      'div[data-test-id="post-content"]',
+    ],
 
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
-    transforms: {},
+    transforms: {
+      'div[role="img"]': $node => {
+        // External link image preview
+        const $img = $node.find('img');
+        const bgImg = $node.css('background-image');
+        if ($img.length === 1 && bgImg) {
+          $img.attr('src', bgImg.match(/\((.*?)\)/)[1].replace(/('|")/g, ''));
+          return $img;
+        }
+        return $node;
+      },
+    },
 
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
     // the result
-    clean: [
-      '._23h0-EcaBUorIHC-JZyh6J',
-      '.ak6zgf-0',
-      '.s5mir6g-0',
-      '._1hwEKkB_38tIoal6fcdrt9',
-    ],
+    clean: ['.icon'],
   },
 };
--- src/extractors/custom/www.reddit.com/index.test.js
+++ src/extractors/custom/www.reddit.com/index.test.js
@@ -71,16 +71,25 @@ describe('WwwRedditComExtractor', () => {
     });
 
     it('returns the lead_image_url', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069933451.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/aww/comments/aybw1m/nothing_to_see_here_human/';
+
       // To pass this test, fill out the lead_image_url selector
       // in ./src/extractors/custom/www.reddit.com/index.js.
-      const { lead_image_url } = await result;
+      const { lead_image_url } = await Mercury.parse(uri, { html });
 
       // Update these values with the expected values from
       // the article.
-      assert.equal(lead_image_url, null);
+      assert.equal(
+        lead_image_url,
+        'https://preview.redd.it/jsc4t74psok21.jpg?auto=webp&s=9c9826487e34d399333f65beb64390206fff4125'
+      );
     });
 
-    it('returns the content', async () => {
+    it('returns the content for text posts', async () => {
       // To pass this test, fill out the content selector
       // in ./src/extractors/custom/www.reddit.com/index.js.
       // You may also want to make use of the clean and transform
@@ -102,6 +111,122 @@ describe('WwwRedditComExtractor', () => {
         first13,
         'Edit: thank you for educating me about the ubiquity of vanilla. Still, none'
       );
+    });
+
+    it('handles posts that only have a title', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069905710.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/AskReddit/comments/axtih6/what_is_the_most_worth_it_item_you_have_ever/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      assert.equal(content, '<div></div>');
+    });
+
+    it('handles image posts', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069933451.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/aww/comments/aybw1m/nothing_to_see_here_human/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      const $ = cheerio.load(content || '');
+
+      const image = $(
+        'img[src="https://preview.redd.it/jsc4t74psok21.jpg?width=960&crop=smart&auto=webp&s=54349b21ff628e8c22c053509e86ba84ff9751d3"]'
+      );
+
+      assert.equal(image.length, 1);
+    });
+
+    it('handles video posts', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069947100.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/HumansBeingBros/comments/aybtf7/thanks_human/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      const $ = cheerio.load(content || '');
+
+      const video = $(
+        'video > source[src="https://v.redd.it/kwhzxoz5rok21/HLSPlaylist.m3u8"]'
+      );
+
+      assert.equal(video.length, 1);
+    });
+
+    it('handles external link posts with image preview', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069958273.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/todayilearned/comments/aycizd/til_that_when_jrr_tolkiens_son_michael_signed_up/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      const $ = cheerio.load(content || '');
+
+      const link = $(
+        'a[href="https://www.1843magazine.com/culture/look-closer/tolkiens-drawings-reveal-a-wizard-at-work"]'
+      );
+
+      const image = $(
+        'img[src="https://b.thumbs.redditmedia.com/gWPmq95XmEzQns6B-H6_l4kBNFeuhScpVDYPjvPsdDs.jpg"]'
+      );
+
+      assert.equal(link.length, 2);
+
+      assert.equal(image.length, 1);
+    });
+
+    it('handles external image posts with image preview', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552070031501.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/gifs/comments/4vv0sa/leonardo_dicaprio_scaring_jonah_hill_on_the/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      const $ = cheerio.load(content || '');
+
+      const link = $('a[href="http://i.imgur.com/Qcx1DSD.gifv"]');
+
+      const image = $(
+        'img[src="https://external-preview.redd.it/sKJFPLamiRPOW5u7NTch3ykbFYMwqI5Qr0zlCINMTfU.gif?format=png8&s=56ecd472f8b8b2ee741b3b1cb76cb3a5110a85f9"]'
+      );
+
+      assert.equal(link.length, 1);
+
+      assert.equal(image.length, 1);
+    });
+
+    it('handles external link posts with embedded media', async () => {
+      const html = fs.readFileSync(
+        './fixtures/www.reddit.com/1552069973740.html'
+      );
+      const uri =
+        'https://www.reddit.com/r/videos/comments/5gafop/rick_astley_never_gonna_give_you_up_sped_up_every/';
+
+      const { content } = await Mercury.parse(uri, { html });
+
+      const $ = cheerio.load(content || '');
+
+      const link = $('a[href="https://youtu.be/dQw4w9WgXcQ"]');
+
+      const embed = $(
+        'iframe[src="https://www.redditmedia.com/mediaembed/5gafop?responsive=true"]'
+      );
+
+      assert.equal(link.length, 1);
+
+      assert.equal(embed.length, 1);
     });
   });
 });
