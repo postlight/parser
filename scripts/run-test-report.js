7cebf38100846213027057088118bfa20b89ea58
Frankie Simms <fdsimms@gmail.com>
2019-03-27 15:58:36 -0400

    fix: update pathname

--- scripts/run-test-report.js
+++ scripts/run-test-report.js
@@ -2,4 +2,4 @@ const path = require('path');
 
 const { comment } = require('@postlight/ci-failed-test-reporter');
 
-comment(path.resolve('/test-output.json'));
+comment(path.join(__dirname, '../', '/test-output.json'));
