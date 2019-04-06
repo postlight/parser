a3426cba55f2c88f470d043cca240060cb82873e
kik0220 <kik0220@gmail.com>
2019-04-06 23:26:11 +0900

    feat: add www.moongift.jp custom parser

---
+++ src/extractors/custom/www.moongift.jp/index.js
@@ -0,0 +1,27 @@
+export const WwwMoongiftJpExtractor = {
+  domain: 'www.moongift.jp',
+
+  title: {
+    selectors: ['h1.title a'],
+  },
+
+  author: null,
+
+  date_published: null,
+
+  dek: {
+    selectors: [['meta[name="og:description"]', 'value']],
+  },
+
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']],
+  },
+
+  content: {
+    selectors: ['#main'],
+
+    transforms: {},
+
+    clean: ['ul.mg_service.cf'],
+  },
+};
