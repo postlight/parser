a5f06ce27aa745d5898758df58d5fe4f6fda0b80
kik0220 <4346449+kik0220@users.noreply.github.com>
2019-04-13 00:11:05 +0900

    feat: add takagi-hiromitsu.jp custom parser (#364)

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -110,3 +110,4 @@ export * from './www.infoq.com';
 export * from './www.moongift.jp';
 export * from './www.itmedia.co.jp';
 export * from './www.publickey1.jp';
+export * from './takagi-hiromitsu.jp';
