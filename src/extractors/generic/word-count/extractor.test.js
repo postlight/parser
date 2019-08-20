b1bc0618db29151a820339e06e0f5a0ac0f5b532
Theo Lampert <theolampert@gmail.com>
2019-08-20 09:33:30 +0200

    add <main> test case

--- src/extractors/generic/word-count/extractor.test.js
+++ src/extractors/generic/word-count/extractor.test.js
@@ -17,5 +17,44 @@ describe('GenericWordCountExtractor', () => {
 
       assert.equal(wordCount, 12);
     });
+    it('counts words section', () => {
+      const content = `
+        <section>
+          <p>One two three.</p>
+          <p>Four five six.</p>
+          <p>Seven eight nine.</p>
+          <p>Ten eleven twelve.</p>
+      `;
+
+      const wordCount = GenericWordCountExtractor.extract({ content });
+
+      assert.equal(wordCount, 12);
+    });
+    it('counts words article', () => {
+      const content = `
+        <article>
+          <p>One two three.</p>
+          <p>Four five six.</p>
+          <p>Seven eight nine.</p>
+          <p>Ten eleven twelve.</p>
+      `;
+
+      const wordCount = GenericWordCountExtractor.extract({ content });
+
+      assert.equal(wordCount, 12);
+    });
+    it('counts words article', () => {
+      const content = `
+        <main>
+          <p>One two three.</p>
+          <p>Four five six.</p>
+          <p>Seven eight nine.</p>
+          <p>Ten eleven twelve.</p>
+      `;
+
+      const wordCount = GenericWordCountExtractor.extract({ content });
+
+      assert.equal(wordCount, 12);
+    });
   });
 });
