217cf25ed3539a2ceb1060d3af3dc1cae9ea4a20
Theo Lampert <theolampert@gmail.com>
2019-08-20 09:33:10 +0200

    extend word extractor

--- src/extractors/generic/word-count/extractor.js
+++ src/extractors/generic/word-count/extractor.js
@@ -5,7 +5,7 @@ import { normalizeSpaces } from 'utils/text';
 const GenericWordCountExtractor = {
   extract({ content }) {
     const $ = cheerio.load(content);
-    const $content = $('div').first();
+    const $content = $('div, section, article, main').first();
 
     const text = normalizeSpaces($content.text());
     return text.split(/\s/).length;
