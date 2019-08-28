2422e4717d1067768305e25a56510cc58d4b792d
Michael Ashley <ashley.michaelt@gmail.com>
2019-08-28 07:04:27 -0700

    fix: incorrect parsing on medium.com (#477)
    * fix: medium extractor now pulls content
    
    * fix: remove youtube caption if no preview available
    
    * fix: remove youtube node if no image
    
    * fix: removing dek from medium.com extractor

--- src/extractors/custom/medium.com/index.js
+++ src/extractors/custom/medium.com/index.js
@@ -1,10 +1,8 @@
 export const MediumExtractor = {
   domain: 'medium.com',
 
-  supportedDomains: ['trackchanges.postlight.com'],
-
   title: {
-    selectors: ['h1'],
+    selectors: ['h1', ['meta[name="og:title"]', 'value']],
   },
 
   author: {
@@ -12,11 +10,7 @@ export const MediumExtractor = {
   },
 
   content: {
-    selectors: [
-      ['.section-content'],
-      '.section-content',
-      'article > div > section',
-    ],
+    selectors: ['article'],
 
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
@@ -25,14 +19,18 @@ export const MediumExtractor = {
       iframe: $node => {
         const ytRe = /https:\/\/i.embed.ly\/.+url=https:\/\/i\.ytimg\.com\/vi\/(\w+)\//;
         const thumb = decodeURIComponent($node.attr('data-thumbnail'));
+        const $parent = $node.parents('figure');
 
         if (ytRe.test(thumb)) {
           const [_, youtubeId] = thumb.match(ytRe); // eslint-disable-line
           $node.attr('src', `https://www.youtube.com/embed/${youtubeId}`);
-          const $parent = $node.parents('figure');
           const $caption = $parent.find('figcaption');
           $parent.empty().append([$node, $caption]);
+          return;
         }
+
+        // If we can't draw the YouTube preview, remove the figure.
+        $parent.remove();
       },
 
       // rewrite figures to pull out image and caption, remove rest
@@ -42,29 +40,33 @@ export const MediumExtractor = {
 
         const $img = $node.find('img').slice(-1)[0];
         const $caption = $node.find('figcaption');
+
         $node.empty().append([$img, $caption]);
+      },
+
+      // Remove any smaller images that did not get caught by the generic image
+      // cleaner (author photo 48px, leading sentence images 79px, etc.).
+      img: $node => {
+        const width = parseInt($node.attr('width'), 10);
+        if (width < 100) $node.remove();
       },
     },
 
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
     // the result
-    clean: [],
+    clean: ['span', 'svg'],
   },
 
   date_published: {
-    selectors: [['time[datetime]', 'datetime']],
+    selectors: [['meta[name="article:published_time"]', 'value']],
   },
 
   lead_image_url: {
     selectors: [['meta[name="og:image"]', 'value']],
   },
 
-  dek: {
-    selectors: [
-      // enter selectors
-    ],
-  },
+  dek: null,
 
   next_page_url: {
     selectors: [
