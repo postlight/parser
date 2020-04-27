9c93f9ec2589541a41e2996beb63a4d1b1b89c60
John Brayton <jbrayton@goldenhillsoftware.com>
2020-04-27 08:17:05 -0400

    Clarified comments.

--- src/extractors/custom/arstechnica.com/index.js
+++ src/extractors/custom/arstechnica.com/index.js
@@ -1,6 +1,12 @@
 export const ArstechnicaComExtractor = {
   domain: 'arstechnica.com',
 
+  // Articles from this site are often paginated, but I was unable to write a CSS
+  // selector to find the next page. On the last page, there will be a link with a CSS
+  // selector indicating that the previous page is next. But the parser appears to find
+  // the next page without this extractor finding it, as long as the fallback option is
+  // left at its default value of true.
+
   title: {
     selectors: ['title'],
   },
@@ -28,13 +34,16 @@ export const ArstechnicaComExtractor = {
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
       h2: $node => {
+        // Some pages have an element h2 that is significant, and that the parser will
+        // remove if not following a paragraph. Adding this empty paragraph fixes it, and
+        // the empty paragraph will be removed anyway.
         $node.before('<p></p>');
       },
     },
 
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
-    // the result
+    // the result.
     clean: [
       // Remove enlarge links and separators inside image captions.
       'figcaption .enlarge-link',
