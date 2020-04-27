3093a60a702463d1a8ba19d918b75daa120fc3d4
John Brayton <jbrayton@goldenhillsoftware.com>
2020-04-27 19:03:04 -0400

    extractor - works for one article, but not another yet...

---
+++ src/extractors/custom/www.pilzforum.at/index.js
@@ -0,0 +1,28 @@
+export const WwwPilzforumAtExtractor = {
+  domain: 'www.pilzforum.at',
+
+  title: {
+    selectors: ['title'],
+  },
+
+  author: {
+    selectors: ['div.author_information strong span'],
+  },
+
+  date_published: {
+    selectors: [['.post_date span', 'title']],
+  },
+
+  content: {
+    selectors: ['.post_content'],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: ['.post_head'],
+  },
+};
