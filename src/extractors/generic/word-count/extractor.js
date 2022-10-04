94af8c3e7f6f22994e30d44e1f43762cc41ba40a
Mike Ashley <ashley.michaelt@gmail.com>
2022-10-03 23:54:52 -0500

    fix: add alternative word count method

--- src/extractors/generic/word-count/extractor.js
+++ src/extractors/generic/word-count/extractor.js
@@ -2,13 +2,25 @@ import cheerio from 'cheerio';
 
 import { normalizeSpaces } from 'utils/text';
 
+const getWordCount = content => {
+  const $ = cheerio.load(content);
+  const $content = $('div').first();
+  const text = normalizeSpaces($content.text());
+  return text.split(/\s/).length;
+};
+
+const getWordCountAlt = content => {
+  content = content.replace(/<[^>]*>/g, ' ');
+  content = content.replace(/\s+/g, ' ');
+  content = content.trim();
+  return content.split(' ').length;
+};
+
 const GenericWordCountExtractor = {
   extract({ content }) {
-    const $ = cheerio.load(content);
-    const $content = $('div').first();
-
-    const text = normalizeSpaces($content.text());
-    return text.split(/\s/).length;
+    let count = getWordCount(content);
+    if (count === 1) count = getWordCountAlt(content);
+    return count;
   },
 };
 
