ef29fa3f57dcb640ee900884726fe7a17bee7c97
Naftali Beder <naftali@naftalibeder.com>
2023-01-23 20:53:08 -0600

    feat: add helper for selecting text slice

--- src/utils/text/excerpt-content.js
+++ src/utils/text/excerpt-content.js
@@ -1,7 +1,20 @@
-export default function excerptContent(content, words = 10) {
+export function excerptContent(content, words = 10) {
   return content
     .trim()
     .split(/\s+/)
     .slice(0, words)
     .join(' ');
 }
+
+export function excerptContentRange(content, start, end) {
+  return content
+    .trim()
+    .split(/\s+/)
+    .slice(start, end)
+    .join(' ');
+}
+
+export default {
+  excerptContent,
+  excerptContentRange,
+};
