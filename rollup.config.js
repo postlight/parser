191c1408b6f72d437dac1fa26538e8bb9004afe9
Naftali Beder <naftali@naftalibeder.com>
2023-01-23 20:41:47 -0600

    dx: add type declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import fs from 'fs';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+
+const copyDeclarations = (src, dest) => {
+  fs.copyFileSync(src, dest);
+};
 
 export default {
   input: 'src/mercury.js',
@@ -10,6 +15,7 @@ export default {
       externalHelpers: false,
       runtimeHelpers: true,
     }),
+    copyDeclarations('src/mercury.d.ts', 'dist/mercury.d.ts'),
   ],
   treeshake: true,
   output: {
